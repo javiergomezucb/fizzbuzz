b2d4991e0973ef6723dd59d2e2a750e3b8272e51
javichosGIT <javier.gomez@ucb.edu.bo>
2026-08-27 10:02:35 -0400

    feat: verificacion del numero divisible entre 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,5 @@
 function fizzbuzz(num) {
-     if (num === 5) {
+     if (num % 5 === 0 ) {
         return "buzz";
     }
 
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -5,7 +5,6 @@ describe("fizzbuzz", () => {
     expect(fizzbuzz(2)).toEqual("2");
   });
 
-
   it("deberia retornar otro numero que no sigue una regla", () => {
     expect(fizzbuzz(1)).toEqual("1");
   });
@@ -22,4 +21,11 @@ describe("fizzbuzz", () => {
     expect(fizzbuzz(5)).toEqual("buzz");
   });
 
+    it("deberia retornar buzz si el numero es divisible por 5", () => {
+    expect(fizzbuzz(10)).toEqual("buzz");
+  });
+
+   it("deberia retornar fizzbuzz si el numero es divisible por 3 y 5", () => {
+    expect(fizzbuzz(15)).toEqual("fizzbuzz");
+  });
 });
